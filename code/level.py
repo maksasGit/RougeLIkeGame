26878202b0c6c165025495e24bf0d70d6b5b01e5
maksasGit <maksim.rishko.vit@gmail.com>
2022-12-02 12:53:02 +0100

    -start -window -player -movement -tool -enemy

--- code/level.py
+++ code/level.py
@@ -49,3 +49,8 @@ class CameraGroup(pygame.sprite.Group):
                     offset_rect = sprite.rect.copy()
                     offset_rect.center -= self.offset
                     self.display_surface.blit(sprite.image, offset_rect)
+
+
+
+    def GitHubTest(self):
+        print("Gihub")
